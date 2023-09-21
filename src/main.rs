7936a8080e306d2a0b213f45da986678c12299a2
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-09-21 15:39:04 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,7 @@ pub fn cast_ray(ray: &Ray, scene: &Scene, depth: u32) -> Vec3f {
 pub fn main() {
     const WIDTH: u32 = 640;
     const HEIGHT: u32 = 480;
-    const SAMPLES: u32 = 256;
+    const SAMPLES: u32 = 2048;
     const BOUNCES: u32 = 3;
 
     let camera = Camera::new(Vec3f::new(0.0, 0.0, 0.0), (WIDTH, HEIGHT));
