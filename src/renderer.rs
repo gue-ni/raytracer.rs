a5a184fc73686de04f8d0e7269837e65a3f55d7f
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-10-06 09:36:35 +0200

    Update renderer.rs

--- src/renderer.rs
+++ src/renderer.rs
@@ -57,29 +57,29 @@ impl Renderer {
             let (direction, distance, normal) = Self::sample_light(&light, hit.point);
             let shadow_ray = Ray::new(hit.point, direction);
 
+            let cos_theta = Vec3::dot(normal, -direction);
+
             let closest = scene.hit(&shadow_ray, 0.001, f64::INFINITY);
 
-            if closest.is_none() || closest.t > distance {
+            if (closest.is_none() || closest.t > distance) && 0.0 < cos_theta {
                 let emission = light.emission;
-                let cos_theta = Vec3::dot(hit.normal, direction).abs();
 
                 let pdf = {
                     let distance2 = distance * distance;
-                    let radius2 = light.geometry.radius * light.geometry.radius;
-                    let area = 4.0 * PI * radius2;
+                    let area = light.geometry.surface_area();
                     distance2 / (area * cos_theta)
                 };
 
                 let bsdf = material.bsdf(hit.normal, wo, direction);
 
-                direct_light += bsdf * emission / (pdf);
+                direct_light += bsdf * emission / pdf;
             }
         }
 
         direct_light
     }
 
-    fn path_tracing_dls(ray: &Ray, scene: &Scene, bounce: u32) -> Vec3f {
+    fn path_tracing(ray: &Ray, scene: &Scene, bounce: u32) -> Vec3f {
         if let Some(hit) = scene.hit(ray, 0.001, f64::INFINITY) {
             let material = scene.objects[hit.idx].material;
             let point = hit.point + hit.normal * 0.001;
@@ -95,157 +95,10 @@ impl Renderer {
                 let bsdf = material.bsdf(hit.normal, wo, wi);
                 let cos_theta = Vec3::dot(hit.normal, wi).abs();
                 let ray = Ray::new(point, wi);
-                color += Self::path_tracing_dls(&ray, scene, bounce - 1) * bsdf * cos_theta / pdf;
+                color += Self::path_tracing(&ray, scene, bounce - 1) * bsdf * cos_theta / pdf;
             }
 
             color
-        } else {
-            scene.background
-        }
-    }
-
-    /// Path Tracing with Explicit/Direct Light Sampling
-    /// https://computergraphics.stackexchange.com/questions/5152/progressive-path-tracing-with-explicit-light-sampling?noredirect=1&lq=1
-    /// https://computergraphics.stackexchange.com/questions/4288/path-weight-for-direct-light-sampling
-    /// For some reason this only works if the entire light sphere is visible
-    #[allow(dead_code)]
-    fn path_tracing_nee(incident: &Ray, scene: &Scene, max_bounce: u32) -> Vec3f {
-        let mut radiance = Vec3::from(0.0);
-        let mut throughput = Vec3::from(1.0);
-
-        let mut ray = incident.clone();
-
-        for bounce in 0..max_bounce {
-            if let Some(hit) = scene.hit(&ray, 0.001, f64::INFINITY) {
-                let material = scene.objects[hit.idx].material;
-
-                let point = hit.point + hit.normal * 0.001;
-                let wi = Onb::local_to_world(hit.normal, cosine_weighted_hemisphere());
-
-                ray = Ray::new(point, wi);
-
-                if bounce == 0 {
-                    radiance += throughput * (material.albedo * material.emittance);
-                }
-
-                throughput *= material.albedo;
-
-                assert_eq!(scene.lights.len(), 1);
-
-                for &i in &scene.lights {
-                    if hit.idx != i {
-                        let light = scene.objects[i];
-                        let emission = light.material.albedo * light.material.emittance;
-
-                        // sample point on light
-                        let (point_on_light, light_normal) = {
-                            let normal = point_on_sphere().normalize();
-                            let point = light.geometry.center + normal * light.geometry.radius;
-                            (point, normal)
-                        };
-
-                        let shadow_ray = Ray::new(point, Vec3::normalize(point_on_light - point));
-
-                        if let Some(light_hit) = scene.hit(&shadow_ray, 0.001, f64::INFINITY) {
-                            if light_hit.idx == i
-                                && 0.0 < Vec3::dot(light_normal, -shadow_ray.direction)
-                                && bounce < max_bounce - 1
-                            {
-                                if true {
-                                    let cos_theta =
-                                        Vec3::dot(hit.normal, shadow_ray.direction).clamp(0.0, 1.0);
-
-                                    let pdf = {
-                                        let radius2 = light.geometry.radius * light.geometry.radius;
-                                        let area = 4.0 * PI * radius2;
-                                        let distance2 = light_hit.t * light_hit.t;
-                                        let cos_theta_light =
-                                            Vec3::dot(light_normal, -shadow_ray.direction)
-                                                .clamp(0.0, 1.0);
-                                        distance2 / (cos_theta_light * area)
-                                    };
-
-                                    let brdf = material.albedo / PI;
-
-                                    radiance += throughput * emission * cos_theta * brdf / pdf;
-                                    //radiance += throughput * emission * cos_theta * brdf / pdf;
-                                } else {
-                                    let weight = {
-                                        let radius2 = light.geometry.radius * light.geometry.radius;
-                                        let distance2 = light_hit.t * light_hit.t;
-
-                                        let cos_a_max =
-                                            f64::sqrt(1.0 - (radius2 / distance2).clamp(0.0, 1.0));
-
-                                        2.0 * (1.0 - cos_a_max)
-                                    };
-
-                                    radiance += (throughput * emission)
-                                        * (weight
-                                            * Vec3::dot(hit.normal, shadow_ray.direction)
-                                                .clamp(0.0, 1.0));
-                                }
-                            }
-                        } else {
-                            //assert!(false);
-                        }
-                    }
-                }
-            } else {
-                radiance += scene.background * throughput;
-                break;
-            }
-        }
-
-        radiance
-    }
-
-    #[allow(dead_code)]
-    fn path_tracing(incident: &Ray, scene: &Scene, max_bounce: u32) -> Vec3f {
-        let mut radiance = Vec3::from(0.0);
-        let mut throughput = Vec3::from(1.0);
-
-        let mut ray = incident.clone();
-
-        for _ in 0..max_bounce {
-            if let Some(hit) = scene.hit(&ray, 0.001, f64::INFINITY) {
-                let material = scene.objects[hit.idx].material;
-                let emittance = material.albedo * material.emittance;
-
-                let (reflected, brdf_multiplier) = material.sample(hit.normal, -ray.direction);
-
-                throughput *= brdf_multiplier;
-                radiance += emittance * throughput;
-
-                ray = Ray::new(hit.point + hit.normal * 0.001, reflected);
-            } else {
-                radiance += scene.background * throughput;
-                break;
-            }
-        }
-
-        radiance
-    }
-
-    /// Naive, unbiased monte-carlo path tracing
-    /// This function implements the rendering equation using monte-carlo integration
-    /// Rendering Equation:
-    /// L(p, wo) = Le + ∫ Li(p, wi) fr(wo, wi) cos(theta) dw
-    /// Monte-Carlo:
-    /// L(p, wo) = Le + 1/N * Σ (fr(wo, wi) * cos(theta) / pdf(wi))
-    #[allow(dead_code)]
-    fn path_tracing_recursive(incident: &Ray, scene: &Scene, depth: u32) -> Vec3f {
-        if depth == 0 {
-            return Vec3::from(0.0);
-        }
-
-        if let Some(hit) = scene.hit(incident, 0.001, f64::INFINITY) {
-            let material = scene.objects[hit.idx].material;
-            let emittance = material.albedo * material.emittance;
-            let (reflected, brdf_multiplier) = material.sample(hit.normal, -incident.direction);
-            let bias = Vec3::dot(reflected, hit.normal).signum() * 0.001;
-            let ray = Ray::new(hit.point + hit.normal * bias, reflected);
-            emittance + Self::path_tracing_recursive(&ray, scene, depth - 1) * brdf_multiplier
         } else {
             scene.background
         }
@@ -300,7 +153,7 @@ impl Renderer {
                         for i in 0..chunk.len() {
                             let xy = get_xy((worker * chunk_size + i) as u32, width);
                             let ray = camera.ray(xy);
-                            let color = Self::path_tracing_dls(&ray, scene, bounces);
+                            let color = Self::path_tracing(&ray, scene, bounces);
                             assert!(0.0 <= f64::min(color.x, f64::min(color.y, color.z)));
                             chunk[i] += color / (samples as f64);
                         }
